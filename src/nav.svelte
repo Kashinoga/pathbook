69d1273d9cde72b677bafc692731f563a31b7711
Kashinoga <kashinoga@github.com>
2022-06-24 00:31:47 -0500

    add favicon

--- src/nav.svelte
+++ src/nav.svelte
@@ -1,7 +1,9 @@
 <div class="navbar bg-base-100">
 	<div class="navbar-start" />
 	<div class="navbar-center">
-		<a class="btn btn-ghost normal-case text-xl" href="/">PB</a>
+		<a class="btn btn-ghost" href="/">
+			<img alt="pb logo" src=icons8-bookmark-stickers-32.png>
+		</a>
 	</div>
 	<div class="navbar-end" />
 </div>
