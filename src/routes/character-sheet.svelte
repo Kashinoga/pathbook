73a181f9b3171e5c2ae82841474d1ff3d33cea49
Kashinoga <kashinoga@github.com>
2022-07-04 00:25:58 -0500

    work on ability scores

--- src/routes/character-sheet.svelte
+++ src/routes/character-sheet.svelte
@@ -1,5 +1,9 @@
 <script>
-	import { playerCharacterGeneral } from '../stores.js';
+	import {
+		playerCharacterAlignments,
+		playerCharacterGeneral,
+		playerCharacterSizes
+	} from '../stores.js';
 
 	/**
 	 * Download
@@ -36,7 +40,6 @@
 		fileText.then((text) => {
 			const yeet = JSON.parse(text);
 			for (const [key, value] of Object.entries(yeet)) {
-				console.log(`${key}: ${value}`);
 				window.localStorage.setItem(`${key}`, `${value}`);
 			}
 		});
@@ -58,7 +61,7 @@
 		>
 	</div>
 
-	<div class="flex flow-row flex-wrap gap-4">
+	<form class="flex flow-row flex-wrap gap-4" id="form">
 		<div class="form-control grow">
 			<label class="input-group input-group-vertical">
 				<span class="py-1">Player Name</span>
@@ -81,13 +84,169 @@
 		</div>
 		<div class="form-control grow">
 			<label class="input-group input-group-vertical">
+				<span class="py-1">Level</span>
+				<input
+					type="number"
+					class="input input-bordered"
+					max="20"
+					bind:value={$playerCharacterGeneral.level}
+				/>
+			</label>
+		</div>
+		<div class="form-control grow">
+			<label class="input-group input-group-vertical">
 				<span class="py-1">Experience Points</span>
 				<input
-					type="text"
+					type="number"
 					class="input input-bordered"
 					bind:value={$playerCharacterGeneral.experiencePoints}
 				/>
 			</label>
 		</div>
-	</div>
+		<div class="form-control grow">
+			<label class="input-group input-group-vertical">
+				<span class="py-1">Ancestry</span>
+				<input
+					type="text"
+					class="input input-bordered"
+					bind:value={$playerCharacterGeneral.ancestry}
+				/>
+			</label>
+		</div>
+		<div class="form-control grow">
+			<label class="input-group input-group-vertical">
+				<span class="py-1">Heritage</span>
+				<input
+					type="text"
+					class="input input-bordered"
+					bind:value={$playerCharacterGeneral.heritage}
+				/>
+			</label>
+		</div>
+		<div class="form-control grow">
+			<label class="input-group input-group-vertical">
+				<span class="py-1">Background</span>
+				<input
+					type="text"
+					class="input input-bordered"
+					bind:value={$playerCharacterGeneral.background}
+				/>
+			</label>
+		</div>
+		<div class="form-control grow">
+			<label class="input-group input-group-vertical">
+				<span class="py-1">Class</span>
+				<input
+					type="text"
+					class="input input-bordered"
+					bind:value={$playerCharacterGeneral.class}
+				/>
+			</label>
+		</div>
+		<div class="form-control grow">
+			<label class="input-group input-group-vertical">
+				<span class="py-1">Size</span>
+				<select class="select select-bordered" bind:value={$playerCharacterGeneral.size}>
+					{#each playerCharacterSizes as size}<option>{size}</option>{/each}
+				</select>
+			</label>
+		</div>
+		<div class="form-control grow">
+			<label class="input-group input-group-vertical">
+				<span class="py-1">Alignment</span>
+				<select class="select select-bordered" bind:value={$playerCharacterGeneral.alignment}>
+					{#each playerCharacterAlignments as alignment}<option>{alignment}</option>{/each}
+				</select>
+			</label>
+		</div>
+		<div class="form-control grow">
+			<label class="input-group input-group-vertical">
+				<span class="py-1">Traits</span>
+				<input
+					type="text"
+					class="input input-bordered"
+					bind:value={$playerCharacterGeneral.traits}
+				/>
+			</label>
+		</div>
+		<div class="form-control grow">
+			<label class="input-group input-group-vertical">
+				<span class="py-1">Deity</span>
+				<input
+					type="text"
+					class="input input-bordered"
+					bind:value={$playerCharacterGeneral.deity}
+				/>
+			</label>
+		</div>
+
+		<div class="flex flex-row gap-4">
+			<div class="flex flex-col gap-4">
+				<div class="form-control grow">
+					<label class="input-group input-group-vertical">
+						<span class="py-1">Strength</span>
+						<input
+							type="number"
+							class="input input-bordered"
+							bind:value={$playerCharacterGeneral.strength}
+						/>
+					</label>
+				</div>
+				<div class="form-control grow">
+					<label class="input-group input-group-vertical">
+						<span class="py-1">Dexterity</span>
+						<input
+							type="number"
+							class="input input-bordered"
+							bind:value={$playerCharacterGeneral.dexterity}
+						/>
+					</label>
+				</div>
+				<div class="form-control grow">
+					<label class="input-group input-group-vertical">
+						<span class="py-1">Constitution</span>
+						<input
+							type="number"
+							class="input input-bordered"
+							bind:value={$playerCharacterGeneral.constitution}
+						/>
+					</label>
+				</div>
+				<div class="form-control grow">
+					<label class="input-group input-group-vertical">
+						<span class="py-1">Wisdom</span>
+						<input
+							type="number"
+							class="input input-bordered"
+							bind:value={$playerCharacterGeneral.wisdom}
+						/>
+					</label>
+				</div>
+				<div class="form-control grow">
+					<label class="input-group input-group-vertical">
+						<span class="py-1">Charisma</span>
+						<input
+							type="number"
+							class="input input-bordered"
+							bind:value={$playerCharacterGeneral.charisma}
+						/>
+					</label>
+				</div>
+			</div>
+			<div class="flex flex-col gap-4 stats stats-vertical shadow">
+				<div class="stat">
+					<div class="stat-title">Strength</div>
+					<div class="stat-value">test</div>
+					<div class="stat-desc">{$playerCharacterGeneral.strength}</div>
+				</div>
+				<div class="stat">
+					<div class="stat-title">Charisma</div>
+					<div class="stat-value">
+						{$playerCharacterGeneral.charismaSign}{$playerCharacterGeneral.charismaMod}
+					</div>
+					<div class="stat-desc">21% more than last month</div>
+				</div>
+			</div>
+		</div>
+	</form>
 </div>
--- src/stores.js
+++ src/stores.js
@@ -4,6 +4,19 @@ import { writable } from 'svelte/store';
 const initialValue = '';
 const initialValueNum = '0';
 
+export const playerCharacterSizes = ['Tiny', 'Small', 'Medium', 'Large', 'Huge', 'Gargantuan'];
+export const playerCharacterAlignments = [
+	'Lawful Good',
+	'Lawful Neutral',
+	'Lawful Evi',
+	'Neutral Good',
+	'Neutral',
+	'Neutral Evil',
+	'Chaotic Good',
+	'Chaotic Neutral',
+	'Chaotic Evil'
+];
+
 export const playerCharacterGeneral = writable({
 	playerName: browser ? window.localStorage.getItem('playerName') ?? initialValue : initialValue, // nullish coalescing operator
 	characterName: browser
@@ -11,13 +24,53 @@ export const playerCharacterGeneral = writable({
 		: initialValue,
 	experiencePoints: browser
 		? window.localStorage.getItem('experiencePoints') ?? initialValueNum
-		: initialValue
+		: initialValueNum,
+	ancestry: browser ? window.localStorage.getItem('ancestry') ?? initialValue : initialValue,
+	heritage: browser ? window.localStorage.getItem('heritage') ?? initialValue : initialValue,
+	background: browser ? window.localStorage.getItem('background') ?? initialValue : initialValue,
+	class: browser ? window.localStorage.getItem('class') ?? initialValue : initialValue,
+	size: browser ? window.localStorage.getItem('size') ?? initialValue : initialValue,
+	alignment: browser ? window.localStorage.getItem('alignment') ?? initialValue : initialValue,
+	traits: browser ? window.localStorage.getItem('traits') ?? initialValue : initialValue,
+	deity: browser ? window.localStorage.getItem('deity') ?? initialValue : initialValue,
+	level: browser ? window.localStorage.getItem('level') ?? initialValue : initialValue,
+	heroPoints: browser ? window.localStorage.getItem('heroPoints') ?? initialValue : initialValue,
+
+	strength: browser ? window.localStorage.getItem('strength') ?? initialValue : initialValue,
+	dexterity: browser ? window.localStorage.getItem('dexterity') ?? initialValue : initialValue,
+	constitution: browser
+		? window.localStorage.getItem('constitution') ?? initialValue
+		: initialValue,
+	intelligence: browser
+		? window.localStorage.getItem('intelligence') ?? initialValue
+		: initialValue,
+	wisdom: browser ? window.localStorage.getItem('wisdom') ?? initialValue : initialValue,
+	charisma: browser ? window.localStorage.getItem('charisma') ?? initialValueNum : initialValueNum,
+	charismaMod: browser
+		? window.localStorage.getItem('charismaMod') ?? initialValueNum
+		: initialValueNum,
+	charismaSign: browser ? window.localStorage.getItem('charismaSign') ?? initialValue : initialValue
 });
 
 playerCharacterGeneral.subscribe((playerCharacterFields) => {
 	if (browser) {
 		for (const [key, value] of Object.entries(playerCharacterFields)) {
 			window.localStorage.setItem(key, value);
+
+			// if (key == 'charisma') {
+			// 	let charismaMod = Math.floor((Number(value) - 10) / 2);
+			// 	console.log(charismaMod.toString());
+			// 	window.localStorage.setItem('charismaMod', charismaMod.toString());
+			// }
 		}
+	}
+});
+
+playerCharacterGeneral.subscribe((test) => {
+	test.charismaMod = Math.floor((Number(test.charisma) - 10) / 2).toString();
+	if (Math.sign(Number(test.charismaMod)) > 0) {
+		test.charismaSign = '+';
+	} else {
+		test.charismaSign = '';
 	}
 });
